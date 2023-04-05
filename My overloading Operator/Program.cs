dfeedbdd27ad9e879bb241f345ea162a89fffe34
Rayna Miller <89815185+UsernameI23@users.noreply.github.com>
2023-04-05 01:37:57 -0400

    Update Program.cs

--- My overloading Operator/Program.cs
+++ My overloading Operator/Program.cs
@@ -1,1 +1,35 @@
 ﻿
+
+
+namespace overloadingList
+{
+    public class List
+    {
+        
+        public int NumberItems { get; set; }
+        public float Type { get; set; }
+
+        public List()
+        {
+            
+            NumberItems = 0;
+            Type = 0;
+        }
+
+        public List(int NumberItems, float TripTIme)
+        {
+           
+            this.NumberItems = NumberItems;
+            this.Type = TripTIme;
+        }
+
+       
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
